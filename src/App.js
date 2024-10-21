dafd904cc48985108f00eacd54513f5e32d9f027
Hyejung Na <hyejung.na01@gmail.com>
2024-10-22 05:37:11 +1100

    Add basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       {/* Router로 Routes감싸주기 */}
       <Routes>
         <Route path="/" element={<TodoPage />} />
